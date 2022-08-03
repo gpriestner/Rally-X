8624feaf4720b7c5f9986f1d6a37b4519c2e707d
Geoff Priestner <Geoff@DN-UK.COM>
2022-08-03 13:20:21 +0100

    Draw a flag

--- index.js
+++ index.js
@@ -178,6 +178,16 @@ class Flag {
     if (sflag) this.image = Picture.Load("/images/Sflagt.png");
     else this.image = Picture.Load("/images/Flagt.png");
   }
+  draw() {
+    const pos = Map.Convert(this.x, this.y);
+
+    Game.View.drawImage(this.image, 0, 0, 24, 24,
+    -0.5 * Car.scale * pos.scale.x, 
+    -0.5 * Car.scale * pos.scale.y, // -10, // pos.y,
+    Car.scale * pos.scale.x,
+    Car.scale * pos.scale.y
+);
+  }
 }
 class Car {
   //  image = Car.Picture("images/bluecar24t2.png");
